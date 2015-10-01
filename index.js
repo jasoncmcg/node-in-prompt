89d7d788924401150d50b0823ed09c452a25eccf
Jason McGinnis <jasoncmcg@yahoo.com>
2015-09-30 23:13:10 -0500

    Starting code
    Starting with tiny cli sample code.

---
+++ index.js
@@ -0,0 +1,25 @@
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('OHAI> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+  switch(line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log('Say what? I might have heard `' + line.trim() + '`');
+      break;
+  }
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
+});
+
+exports.promptStart = function (promptName) {
+	rl.setPrompt(promptName);
+	rl.prompt();
+}
